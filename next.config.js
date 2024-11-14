c9b07808e8660611577b45a3b1b2e65e911ecee9
Rony <ronybarua-ethical18>
2024-11-14 22:20:48 +0600

    next config file is modified

--- next.config.js
+++ next.config.js
@@ -1,40 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: false, // Temporarily disabled to check if it’s causing the issue
-});
-
 const nextConfig = {
-  // Experimental configurations temporarily commented out to test for issues
-  // experimental: {
-  //   optimizePackageImports: ['react-icons'],
-  //   optimizeCss: true,
-  // },
-
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
     ],
   },
-
-  webpack(config) {
-    // Temporarily commenting out custom alias for testing
-    // config.resolve.alias['@ant-design/cssinjs'] = '@ant-design/cssinjs/lib';
-    
-    // Adding logging to identify potential issues
-    console.log("Running Webpack Configuration");
-
-    return config;
-  },
-
-  // Temporarily comment out transpilePackages to test for issues
-  // transpilePackages: ['@ant-design', 'antd'],
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
